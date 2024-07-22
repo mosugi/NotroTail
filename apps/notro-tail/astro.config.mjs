b067253d787dcf9a07745ad2981fdb30aa7cde5b
mosugi <mosugix@gmail.com>
2024-07-22 18:57:29 +0900

    closes #6

--- apps/notro-tail/astro.config.mjs
+++ apps/notro-tail/astro.config.mjs
@@ -1,28 +1,35 @@
-import {defineConfig} from "astro/config";
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import notro from "notro";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-    image: {
-        remotePatterns: [{ protocol: "https" }],
-    },
-    integrations: [
-        tailwind(),
-        notro({
-            token: process.env.NOTION_TOKEN,
-            notionId: process.env.NOTION_ID,
-            optimizeRemoteImage: true,
-            useRawHtml: true,
-            enableCalloutAsWrapper: true,
-            fetchAllPagesOnServerStart: false
-        }),
+  site: 'https://notrotail.mosugi.com',
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
     ],
-    vite: {
-        server: {
-            watch: {
-                ignored: ["**/cache/**/*"],
-            },
-        },
+  },
+  integrations: [
+    tailwind(),
+    sitemap(),
+    notro({
+      token: process.env.NOTION_TOKEN,
+      notionId: process.env.NOTION_ID,
+      optimizeRemoteImage: true,
+      useRawHtml: true,
+      enableCalloutAsWrapper: true,
+      fetchAllPagesOnServerStart: false,
+    }),
+  ],
+  vite: {
+    server: {
+      watch: {
+        ignored: ["**/cache/**/*"],
+      },
     },
+  },
 });
