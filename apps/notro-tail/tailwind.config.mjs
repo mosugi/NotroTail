691211535d4ced2a08881bd7dfa02aae30d50f24
mosugi <mosugix@gmail.com>
2024-07-23 09:16:33 +0900

    #15 Enable fetching all pages on server start Add usePolling and interval options for tailwindcss class loading in Notion

--- apps/notro-tail/tailwind.config.mjs
+++ apps/notro-tail/tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}",
     "../../node_modules/notro/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", // Notro components (for monorepo)
     "./node_modules/notro/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", // Notro components
     "./cache/**/*.json" // Classes used in Notion
